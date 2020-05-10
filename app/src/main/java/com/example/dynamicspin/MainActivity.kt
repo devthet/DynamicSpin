fe08a5e8d363543e76f40896ed483a0dca1cf6f8
devthet <devthet@gmail.com>
2020-05-10 22:27:13 +0630

    add spinner

--- app/src/main/java/com/example/dynamicspin/MainActivity.kt
+++ app/src/main/java/com/example/dynamicspin/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.dynamicspin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //access the items
+        val languages = arrayListOf("Java","Kotlin","Swift","C#")
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if(spinner!= null){
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, languages)
+            spinner.adapter = adapter
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    Toast.makeText(this@MainActivity,getString(R.string.selected_item)+" "+ " "+languages[position],Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+        }
     }
 }
